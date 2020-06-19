8da7c04b12cf5283041017263de994051394e73c
bbarnhi <barnhill0126@gmail.com>
2020-06-19 13:41:47 -0400

    noon commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 ///////////////////////////////////////
@@ -17,7 +17,7 @@ import './App.css';
 
 ///////////Psuedo Code -- Basic Functionality
 //  Fetch data from USAF-sdi-react-checkpoint
-//  Example email
+//  Example email -- from http://localhost:3001/emails
 //      {
 //        "sender": "katie@galvanize.com", 
 //        "recipient": "jane@galvanize.com" , 
@@ -43,59 +43,91 @@ import './App.css';
 //   Send button that onclick processes email.
 // /////////
 
+// var fetch = require('node-fetch') 
+//no idea what this is supposed to do commenting out till needed. 
+
 // Setup Class constructor for an email
-
-var fetch = require('node-fetch')
-
 class Email extends React.Component{
   constructor(props){
     super(props)
     // things to fetch
+    console.log("Made it to ClassBuilder")
+    this.state ={
+      data: null
+    }
     this.sender = null;
     this.recipient = null;
     this.subject = null;
     this.message = null;
     this.date = null;
     this.id = null;  //primary key
+    //  call fetch?
+    this.fetchEmails = this.fetchEmails.bind(this)  //why is this needed?
+    this.fetchEmails()
   }
+
+async fetchEmails(){ 
+  console.log("Made it to fetchEmails function")
+  // Magic youTube example setup
+  // const url = "http://localhost:3001/emails";     // THIS BREAKS
+  // // const url = "https://api.randomuser.me/";    ///THIS WORKS
+  // const response = await fetch(url);
+  // const data = await response.json();
+  // console.log(data);
+
+
+  //back to basics... fetch setup 
+  fetch('http://localhost:3001/emails') ///THIS BREAKS
+  //fetch('https://api.randomuser.me/') // THIS WORKS
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  //whats different in the data sets.  Emails is an Array of objects.   Randomuser is just 1 object. 
+
+
+//   //console.log("response: " + response)
+// //  let data = await response.json();
+//   //let data = response.json();
+//   // should populate data with an Array of 19 Objects each with 6 properties 
+//   console.log("data array: " + this.data)
+//   console.log("data array: " + this.sender)
+//   // set this data for incoming emails
+//   // this.sender = data.sender;
+//   // this.recipient = data.recipient;
+//   // this.subject = data.subject;
+//   // this.message = data.message;
+//   // this.date = data.date;
+//   // this.id = data.id;
+//   console.log(this.sender)
 }
 
-async fetchEmails(){
-  let url = `http://localhost:3001/api`;
-  let response = await fetch(url);
-  let data = await response.json();
-  // set this data for incoming emails
-  this.sender = data.sender;
-  this.recipient = data.recipient;
-  this.subject = data.subject;
-  this.message = data.message;
-  this.date = data.date;
-  this.id = date.id;
-}
 // Onlick filter returned emails based on this.sender
 filterSender(event){
+  console.log("Made it to filterSender function")
   let currentFilter = this.sender
   // no idea how to implent this -- search through fetch emails based on indexOf(passed sender name)
-  return emails.indexOf(currentFilter)
+  //return emails.indexOf(currentFilter)
 }
 openEmail(event){
+  console.log("Made it to openEmail function")
   let currentEmail = this.id //grab the id of the selected email
   //open a modul with 4 rows
-  <div>
-    <div> this.subject </div>
-    <div> this.sender </div><div> this.date</div>
-    <div> this.message </div>
+  /*<div>
+     <div>{this.subject} </div>
+    <div>{this.sender}</div><div>{this.date}</div>
+    <div>{this.message}</div>
     <button>Reply</button><button>Reply All</button><button>Forward</button><button>Delete</button>
-  </div>
+  </div> */
 }
 newEmail(event){
+  console.log("Made it to newEmail function")
   //open modul with 3 forms + submit button
-  <div>
-    <form className="recipient"></form>
-    <form className="subject"></form>
-    <form className="message"></form>
-    <button>Submit</button><button>Cancel</button>
-  </div>
+  // <div>
+  //   <form className="recipient"></form>
+  //   <form className="subject"></form>
+  //   <form className="message"></form>
+  //   <button>Submit</button><button>Cancel</button>
+  // </div>
 }
 
 //Screen needs to be split into chunks
@@ -107,44 +139,45 @@ newEmail(event){
 //   email  |[  Sender | Subject  | Date |  ] - Item 4
 // Folders? |[  Sender | Subject  | Date |  ] - Item 5
 
-
+//       http://localhost:3001/emails.id[1]
 render() {
+  console.log("Made it to Render function")
   return (
     <div>
       <div> <h1>Barnhill Email Systems </h1>
       <div className="searchBar"></div>
       <div className="mainEmail">
-        <div className="col1 sender" onClick={this.filterSender}>{this.sender}</div>
-        <div className="col2 subject" onClick={this.openEmail}>{this.subject}</div>
-        <div className="col3 date">{this.date}</div>
+        <div className="col1 sender" onClick={this.filterSender}>Sender: {this.sender}</div>
+        <div className="col2 subject" onClick={this.openEmail}>Subject: {this.subject}</div>
+        <div className="col3 date">Date: {this.date}</div>
       </div>
       <div className="newEmail">
         <button onClick={this.newEmail}>Compose</button>
       </div>
-      
+
       </div>
     </div>
   )
 }
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+};
+export default Email;
